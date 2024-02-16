573114dba1418d2b91771cc449426ebf5e39c088
Augusto Gonçalves <pri.augusto@voeazul.com.br>
2024-02-16 10:25:27 -0300

    feat: add the function add new contatct

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # address_book
+
+## Regras da aplicação
+
+- A aplicação deve iniciar mostrando uma lista de opções do que é possível fazer com o app e permitir que o usuário digite uma escolha para iniciar a aplicação.
+- Deve ser possível adicionar um contato
+    - O contato pode ter os dados:
+    - Nome
+    - Telefone
+    - Email
+    - Favorito (está opção é para poder marcar um contato como favorito)
+- Deve ser possível visualizar a lista de contatos cadastrados
+- Deve ser possível editar um contato
+- Deve ser possível marcar/desmarcar um contato como favorito
+- Deve ser possível ver uma lista de contatos favoritos
+- Deve ser possível apagar um contato
---
+++ server/manager.py
@@ -0,0 +1,51 @@
+
+
+def add_new_contact(contacts, new_contact, cellphone_number, email):
+    contact_info = {"contact": new_contact, "cellphone": cellphone_number, "email": email, "favorite": False}
+    contacts.append(contact_info)
+    print(f"\n Contato {new_contact} , {cellphone_number} , {email} foi adicionado com sucesso! ✅")
+    print(contacts)
+    return
+
+contacts = []
+while True:
+    print("\n Agenda de Contatos")
+    print("1. Adicionar contato")
+    print("2. Visualizar lista de contatos")
+    print("3. Editar contato")
+    print("4. Marcar contato como favorito")
+    print("5. Desmarcar favorito")
+    print("6. Visualizar lista de favoritos")
+    print("7. Deletar contato")
+    print("8. Sair do programa")
+
+    choice = input("\n Digite sua escolha: ")
+
+    if choice == "1":
+        new_contact = input("Digite o nome do contato Āβ: ")
+        cellphone_number = input("Digite o número do contato 📱: ")
+        email = input("Digite o email do contato ✉️: ")
+        add_new_contact(contacts, new_contact, cellphone_number, email)
+
+    elif choice == "2":
+        print("Choice 2 selected!")
+
+    elif choice == "3":
+        print("Choice 3 selected!")
+
+    elif choice == "4":
+        print("Choice 4 selected!")
+
+    elif choice == "5":
+        print("Choice 5 selected!")
+
+    elif choice == "6":
+        print("Choice 6 selected!")
+
+    elif choice == "7":
+        print("Choice 4 selected!")
+
+    elif choice == "8":
+        break
+
+print("\n Você saiu da agenda 😞")
